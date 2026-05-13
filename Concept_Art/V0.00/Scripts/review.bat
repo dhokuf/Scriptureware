fcdd5c874c04754a6cb819b83cad219ac783f6a7
dhokuf <90402170+dhokuf@users.noreply.github.com>
2026-05-13 16:27:07 -0400

    Another big, beautiful batch file

---
+++ Concept_Art/V0.00/Scripts/review.bat
@@ -0,0 +1,21 @@
+@echo off
+echo ---------- Scriptureware! ----------"
+echo Version  0.00 (alpha) 
+echo Created by David Hokuf and Benjamin Van Grouw
+echo Last update: May 2026
+echo Scriptureware currently supports:
+echo     ^<1^> 1 Peter [5 chapters]
+echo Enter the index of the book to memorize or ^<quit^> to exit: 1
+echo Enter ^<1^> to review, ^<2^> to memorize new material, or ^<quit^> to exit: 1
+echo Enter a reference to begin review (e.g. <1:1>): 1:1
+echo ---------- Reviewing 1 Peter 1:1 ----------
+echo Enter each verse as prompted. Enter ^<quit^> to return to the main menu.
+echo 1:1 ^> Peter, an apostle of Jesus Christ, To those who are elect exiles of the Dispersion in Pontus, Galatia, Cappadocia, Asia, and Bithynia,
+echo 1:2 ^> according to the foreknowledge of God the Father, in the sanctification of the Spirit, for obedience to Jesus Christ and for sprinkling with his blood.
+echo Incorrect! 1 Peter 1:2:
+echo "according to the foreknowledge of God the Father, in the sanctification of the Spirit, for obedience to Jesus Christ and for sprinkling with his blood: May grace and peace be multiplied to you."
+echo 1:2 ^> quit
+echo ---------- Exiting Review ----------
+echo Review Stats: You reviewed 1 Peter 1:1 - 1:2 with 1/2 verses correct (50% accuracy).
+echo Enter ^<1^> to review, ^<2^> to memorize new material, or ^<quit^> to exit: quit
+timeout /t 50 > nul
