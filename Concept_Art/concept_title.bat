9c6350fb67cd6adb588a8dce3dd62dcc26f14ebb
dhokuf <90402170+dhokuf@users.noreply.github.com>
2026-05-13 16:00:52 -0400

    The first script arrives

---
+++ Concept_Art/concept_title.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ---------- Scriptureware! ----------
+echo Version  0.00 (alpha) 
+echo Created by David Hokuf and Benjamin Van Grouw
+echo Last update: May 2026
+echo Scriptureware currently supports:
+echo     ^<1^> 1 Peter [5 chapters]
+echo Enter the index of the book to memorize or ^<quit^> to exit: quit
+timeout /t 50 > nul
