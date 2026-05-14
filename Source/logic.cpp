59d39d73d442cdab257ba44349383d4fec3b8f8b
David Hokuf <david.hokuf@gmail.com>
2026-05-14 12:57:54 +0000

    The actual file structure makes it onto the scene

---
+++ Source/logic.cpp
@@ -0,0 +1,7 @@
+/* 
+---------- Scriptureware ----------
+Version  0.00 (alpha)
+Authors: David Hokuf and Benjamin Van Grouw
+Date: May 2026 
+*/
+
---
+++ Source/main.cpp
@@ -0,0 +1,16 @@
+/* 
+---------- Scriptureware ----------
+Version  0.00 (alpha)
+Authors: David Hokuf and Benjamin Van Grouw
+Date: May 2026 
+*/
+
+#include "parse.hpp"
+#include "ui.hpp"
+#include "logic.hpp"
+#include "algorithm.hpp"
+
+int main() {
+
+    return 0;
+}
