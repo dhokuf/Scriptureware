072d35c5117adce3fdfb7bb49814e4634e2e6d20
David Hokuf <david.hokuf@gmail.com>
2026-05-14 13:16:25 +0000

    Implemented header comments, debug logging, and build workflow

---
+++ Build/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+g++ -o ./Executables/Scriptureware.exe ../Source/main.cpp ../Source/parse.cpp ../Source/ui.cpp ../Source/logic.cpp ../Source/algorithm.cpp
+./Executables/Scriptureware.exe
--- Source/main.cpp
+++ Source/main.cpp
@@ -5,12 +5,28 @@ Authors: David Hokuf and Benjamin Van Grouw
 Date: May 2026 
 */
 
+// Comment this line out to disable development mode and remove debug output
+/**/ #define _DEVELOPMENT_MODE */
+
+// If development mode is enabled, define a log function for debug output
+#ifdef _DEVELOPMENT_MODE
+    #include <iostream>
+    #define log(x) std::cout << "[DEBUG] " << x << std::endl;
+
+// Otherwise, do nothing when a log statement is encountered
+#else
+    #define log(x)
+
+#endif
+
 #include "parse.hpp"
 #include "ui.hpp"
 #include "logic.hpp"
 #include "algorithm.hpp"
 
+// And so it begins...
 int main() {
 
+    log("Build test success!");
     return 0;
 }
