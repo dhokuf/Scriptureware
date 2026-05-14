1e5397d984c7b1e3d1aaa6d8583ff26e4942564a
David Hokuf <david.hokuf@gmail.com>
2026-05-14 13:19:26 +0000

    Test of build workflow and .gitignore modifications to exclude temp files

--- Build/build.sh
+++ Build/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-g++ -o ./Executables/Scriptureware.exe ../Source/main.cpp ../Source/parse.cpp ../Source/ui.cpp ../Source/logic.cpp ../Source/algorithm.cpp
-./Executables/Scriptureware.exe
+g++ -o Scriptureware.exe ../Source/main.cpp ../Source/parse.cpp ../Source/ui.cpp ../Source/logic.cpp ../Source/algorithm.cpp
+./Scriptureware.exe
