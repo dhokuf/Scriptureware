d9bfde4f58243646b899c224baf136f5906d8466
David Hokuf <david.hokuf@gmail.com>
2026-05-22 16:59:14 -0400

    Update build script

--- Build/build.sh
+++ Build/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-g++ -o Scriptureware.exe ../Source/main.cpp ../Source/ui.cpp ../Source/logic.cpp ../Source/text.cpp
+g++ -o Scriptureware.exe ../Source/main.cpp ../Source/ui.cpp ../Source/logic.cpp ../Source/text.cpp ../Source/verse.cpp
 ./Scriptureware.exe
